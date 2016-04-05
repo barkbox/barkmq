614785b0904d0269998790df5cbb67b1d41472a2
Cris Kim <cris@criskim.com>
2016-04-05 17:25:49 -0400

    add spec for acts as publisher

--- lib/barkmq/acts_as_publisher.rb
+++ lib/barkmq/acts_as_publisher.rb
@@ -9,8 +9,6 @@ module BarkMQ
       def acts_as_publisher(options = {})
         send :include, InstanceMethods
 
-        options[:on] ||= [ :created, :updated, :destroy ]
-
         cattr_accessor :message_serializer
 
         after_commit :after_create_publish, on: :create
@@ -18,12 +16,12 @@ module BarkMQ
         after_commit :after_destroy_publish, on: :destroy
 
         BarkMQ.publisher_config do |c|
-          c.add_topic(self.model_name, 'created')
-          c.add_topic(self.model_name, 'updated')
-          c.add_topic(self.model_name, 'destroyed')
+          c.add_topic(self.model_name.param_key, 'created')
+          c.add_topic(self.model_name.param_key, 'updated')
+          c.add_topic(self.model_name.param_key, 'destroyed')
 
           Array(options[:events]).each do |event|
-            c.add_topic(self.model_name, event)
+            c.add_topic(self.model_name.param_key, event)
           end
         end
 
@@ -49,15 +47,12 @@ module BarkMQ
       end
 
       def after_create_callback
-        puts "after_create_callback"
       end
 
       def after_update_callback
-        puts "after_update_callback"
       end
 
       def after_destroy_callback
-        puts "after_destroy_callback"
       end
 
     end
---
+++ spec/barkmq/acts_as_publisher_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+RSpec.describe 'BarkMQ::ActsAsPublisher' do
+
+  describe '.acts_as_publisher' do
+
+    before do
+      BarkMQ.publisher_config do |c|
+        c.env = 'test'
+        c.app_name = 'barkmq'
+        c.topic_names = []
+      end
+    end
+
+    it 'with no options' do
+      expected_topics = %W{
+        test-barkmq-new_ar_publisher-created
+        test-barkmq-new_ar_publisher-updated
+        test-barkmq-new_ar_publisher-destroyed
+      }
+      class NewArPublisher < ActiveRecord::Base
+        acts_as_publisher
+      end
+      expect(BarkMQ.pub_config.topic_names).to eq(expected_topics)
+    end
+
+    it 'with single custom event string' do
+      expected_topics = %W{
+        test-barkmq-new_ar_publisher-created
+        test-barkmq-new_ar_publisher-updated
+        test-barkmq-new_ar_publisher-destroyed
+        test-barkmq-new_ar_publisher-tested
+      }
+      class NewArPublisher < ActiveRecord::Base
+        acts_as_publisher events: :tested
+      end
+      expect(BarkMQ.pub_config.topic_names).to eq(expected_topics)
+    end
+
+    it 'with single custom event array' do
+      expected_topics = %W{
+        test-barkmq-new_ar_publisher-created
+        test-barkmq-new_ar_publisher-updated
+        test-barkmq-new_ar_publisher-destroyed
+        test-barkmq-new_ar_publisher-tested
+      }
+      class NewArPublisher < ActiveRecord::Base
+        acts_as_publisher events: [ :tested ]
+      end
+      expect(BarkMQ.pub_config.topic_names).to eq(expected_topics)
+    end
+
+    it 'with single custom serializer' do
+      expected_topics = %W{
+        test-barkmq-new_ar_publisher-created
+        test-barkmq-new_ar_publisher-updated
+        test-barkmq-new_ar_publisher-destroyed
+      }
+      class TestSerializer
+      end
+      class NewArPublisher < ActiveRecord::Base
+        acts_as_publisher serializer: TestSerializer
+      end
+      expect(BarkMQ.pub_config.topic_names).to eq(expected_topics)
+      expect(NewArPublisher.message_serializer).to eq(TestSerializer)
+    end
+
+  end
+end

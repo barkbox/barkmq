53788f95a33df72c97d964d58a1a259d6bfab126
Cris Kim <cris@criskim.com>
2016-04-14 10:41:49 -0400

    add more specs

--- spec/barkmq/middleware/datadog_subscriber_logger_spec.rb
+++ spec/barkmq/middleware/datadog_subscriber_logger_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe BarkMQ::Middleware::DatadogSubscriberLogger do
   let(:queue) { 'test_queue' }
   let(:sqs_message) { double(message_id: '123', receipt_handle: 'abc', body: body.to_json) }
   let(:statsd) { Statsd.new }
-  let(:body) { { 'TopicArn' => 'arn:aws:sns:us-east-1:123456789012:test_topic', 'Message' => '{}' } }
+  let(:body) { { "TopicArn" => "arn:aws:sns:us-east-1:123456789012:test_topic", "Message" => "{\"foo\":\"bar\"}" } }
 
   describe '#call' do
     before do
@@ -24,20 +24,28 @@ RSpec.describe BarkMQ::Middleware::DatadogSubscriberLogger do
     describe 'when publisher succeeds' do
       let(:block) { -> {} }
 
+      it 'sets topic and message' do
+        call
+        expect(message_worker.topic).to eq('test_topic')
+        expect(message_worker.message).to eq({"foo" => "bar"})
+      end
+
       it 'instrument to statsd' do
         expect(subject.statsd).to receive(:increment).with('barkmq.message.received', {:tags=>["topic:test_topic"]})
         expect(subject.statsd).to receive(:increment).with('barkmq.message.processed', {:tags=>["topic:test_topic"]})
         expect(subject.statsd).to receive(:gauge).with('barkmq.message.process.time', anything, {:tags=>["topic:test_topic"]})
         call
       end
-
-      it 'set topic and message' do
-        
-      end
     end
 
     describe 'when call fails' do
       let(:block) { ->{ raise StandardError, 'test failure' } }
+
+      it 'sets topic and message' do
+        expect { call }.to raise_error(StandardError)
+        expect(message_worker.topic).to eq('test_topic')
+        expect(message_worker.message).to eq({"foo" => "bar"})
+      end
 
       it 'raises the error' do
         expect { call }.to raise_error(StandardError)

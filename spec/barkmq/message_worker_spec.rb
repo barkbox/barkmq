93f6ccb39279d493aa7406f99df4bd00a75aace7
Cris Kim <cris@criskim.com>
2016-04-14 11:07:18 -0400

    add more specs for message worker

--- spec/barkmq/message_worker_spec.rb
+++ spec/barkmq/message_worker_spec.rb
@@ -5,12 +5,6 @@ RSpec.describe BarkMQ::MessageWorker do
   subject { described_class.new }
 
   describe '#perform' do
-    before do
-      BarkMQ.subscriber_config do |c|
-        c.logger = Logger.new('/tmp/blank.log')
-        c.queue_name = 'test-barkmq'
-      end
-    end
 
     it 'calls BarkMQ.handle_message' do
       expect(subject).to receive(:topic).and_return('test_topic')
@@ -20,7 +14,21 @@ RSpec.describe BarkMQ::MessageWorker do
     end
   end
 
-  # describe 'shoryuken_options' do
-  #
-  # end
+  describe '.shoryuken_options' do
+    it 'sets queue name from BarkMQ.subscriber_config' do
+      BarkMQ.subscriber_config do |c|
+        c.queue_name = 'test-barkmq'
+      end
+      load 'barkmq/message_worker.rb'
+      expect(subject.class.get_shoryuken_options['queue']).to eq('test-barkmq')
+    end
+
+    it 'sets queue name from ENV[\'BARKMQ_QUEUE\']' do
+      cached_barkmq_queue = ENV['BARKMQ_QUEUE']
+      ENV['BARKMQ_QUEUE'] = 'test-env-barkmq'
+      load 'barkmq/message_worker.rb'
+      expect(subject.class.get_shoryuken_options['queue']).to eq('test-env-barkmq')
+      ENV['BARKMQ_QUEUE'] = cached_barkmq_queue
+    end
+  end
 end

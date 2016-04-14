096b9a92f2064439810535ab0dc8d058c5af9dd6
Cris Kim <cris@criskim.com>
2016-04-14 10:59:40 -0400

    more spec coverage

--- spec/barkmq/message_worker_spec.rb
+++ spec/barkmq/message_worker_spec.rb
@@ -2,42 +2,25 @@ require 'spec_helper'
 require 'barkmq/message_worker'
 
 RSpec.describe BarkMQ::MessageWorker do
-  # subject { described_class.new }
+  subject { described_class.new }
+
+  describe '#perform' do
+    before do
+      BarkMQ.subscriber_config do |c|
+        c.logger = Logger.new('/tmp/blank.log')
+        c.queue_name = 'test-barkmq'
+      end
+    end
+
+    it 'calls BarkMQ.handle_message' do
+      expect(subject).to receive(:topic).and_return('test_topic')
+      expect(subject).to receive(:message).and_return('message')
+      expect(BarkMQ).to receive(:handle_message).with('test_topic', 'message')
+      subject.perform('sqs_message', 'body')
+    end
+  end
+
+  # describe 'shoryuken_options' do
   #
-  # let(:message_worker) { BarkMQ::MessageWorker.new }
-  # let(:queue) { 'test_queue' }
-  # let(:sqs_message) { double(message_id: '123', receipt_handle: 'abc', body: body.to_json) }
-  # let(:statsd) { Statsd.new }
-  # let(:body) { { 'TopicArn' => 'arn:aws:sns:us-east-1:123456789012:test_topic', 'Message' => '{}' } }
-  #
-  # describe '#call' do
-  #   before do
-  #     BarkMQ.subscriber_config do |c|
-  #       c.logger = Logger.new('/tmp/blank.log')
-  #     end
-  #   end
-  #
-  #   def call
-  #     subject.call(message_worker, queue, sqs_message, body, &block)
-  #   end
-  #
-  #   describe 'when publisher succeeds' do
-  #     let(:block) { -> {} }
-  #
-  #     it 'instrument to statsd' do
-  #       expect(subject.statsd).to receive(:increment).with('barkmq.message.received', {:tags=>["topic:test_topic"]})
-  #       expect(subject.statsd).to receive(:increment).with('barkmq.message.processed', {:tags=>["topic:test_topic"]})
-  #       expect(subject.statsd).to receive(:gauge).with('barkmq.message.process.time', anything, {:tags=>["topic:test_topic"]})
-  #       call
-  #     end
-  #   end
-  #
-  #   describe 'when call fails' do
-  #     let(:block) { ->{ raise StandardError, 'test failure' } }
-  #
-  #     it 'raises the error' do
-  #       expect { call }.to raise_error(StandardError)
-  #     end
-  #   end
   # end
 end

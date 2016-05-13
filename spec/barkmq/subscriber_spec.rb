ea7209cce5a47040757de8f89c8c1fecd63afc80
Cris Kim <cris@criskim.com>
2016-05-13 14:33:04 -0400

    add topic namespace option

--- spec/barkmq/subscriber_spec.rb
+++ spec/barkmq/subscriber_spec.rb
@@ -21,6 +21,21 @@ RSpec.describe BarkMQ::Subscriber do
       expect(BarkMQ.sub_config.handlers).to eq({ $topics => NewSubscriberWorker })
     end
 
+    it 'accepts a single topic string w namespace' do
+      $topics = 'test_topic_single_string'
+      $namespace = 'other-namespace'
+      $expected_topics = [ $namespace, $topics ].join('-')
+
+      class NewSubscriberWorker
+        include BarkMQ::Subscriber
+        barkmq_subscriber_options topics: $topics,
+                                  namespace: 'other-namespace'
+      end
+
+      expect(BarkMQ.sub_config.topic_names).to eq([$expected_topics])
+      expect(BarkMQ.sub_config.handlers).to eq({ $expected_topics => NewSubscriberWorker })
+    end
+
     it 'accepts a single topic array' do
       $topics = ['test_topic_single_array']
 
@@ -33,6 +48,21 @@ RSpec.describe BarkMQ::Subscriber do
       expect(BarkMQ.sub_config.handlers).to eq(Hash[$topics.collect{ |t| [t, NewSubscriberWorker] }])
     end
 
+    it 'accepts a single topic array w namespace' do
+      $topics = ['test_topic_single_array']
+      $namespace = 'other-namespace'
+      $expected_topics = $topics.collect { |t| [ $namespace, t ].join('-') }
+
+      class NewSubscriberWorker
+        include BarkMQ::Subscriber
+        barkmq_subscriber_options topics: $topics,
+                                  namespace: $namespace
+      end
+
+      expect(BarkMQ.sub_config.topic_names).to eq($expected_topics)
+      expect(BarkMQ.sub_config.handlers).to eq(Hash[$expected_topics.collect{ |t| [t, NewSubscriberWorker] }])
+    end
+
     it 'accepts a multiple topic array' do
       $topics = ['test_topic_1', 'test_topic_2']
 
@@ -43,6 +73,21 @@ RSpec.describe BarkMQ::Subscriber do
 
       expect(BarkMQ.sub_config.topic_names).to eq($topics)
       expect(BarkMQ.sub_config.handlers).to eq(Hash[$topics.collect{ |t| [t, NewSubscriberWorker] }])
+    end
+
+    it 'accepts a multiple topic array w namespace' do
+      $topics = ['test_topic_1', 'test_topic_2']
+      $namespace = 'other-namespace'
+      $expected_topics = $topics.collect { |t| [ $namespace, t ].join('-') }
+
+      class NewSubscriberWorker
+        include BarkMQ::Subscriber
+        barkmq_subscriber_options topics: $topics,
+                                  namespace: $namespace
+      end
+
+      expect(BarkMQ.sub_config.topic_names).to eq($expected_topics)
+      expect(BarkMQ.sub_config.handlers).to eq(Hash[$expected_topics.collect{ |t| [t, NewSubscriberWorker] }])
     end
 
   end
